e661daf859797ef7f7ca3d180455c8316b4fd5e6
Hallworld <prishedko.ilya@yandex.ru>
2023-07-19 14:42:15 +0400

    Create texture file

---
+++ src/Pixijs/Textures.ts
@@ -0,0 +1,9 @@
+import { Texture } from "pixi.js"
+import UnitPNG from "../Chel.png"
+import Rectangle from "../Rectangle.png"
+import Wall from "../Wall.png"
+
+
+export const unit = Texture.from(UnitPNG)
+export const rectangle = Texture.from(Rectangle)
+export const wall = Texture.from(Wall)
