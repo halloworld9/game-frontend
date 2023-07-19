e4db7d7a33a5b713d1d34b443523160344499931
Hallworld <prishedko.ilya@yandex.ru>
2023-07-19 14:43:04 +0400

    Add canvas to html

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
+import { GamePixi } from './Pixijs/GamePixi';
 
 function App() {
+  const myRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    myRef.current?.appendChild(new GamePixi().app.view)
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div ref={myRef}></div>
   );
 }
 
