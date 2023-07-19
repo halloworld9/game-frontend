72e97a55f8e213da674a94937149174bd09ec43a
Hallworld <prishedko.ilya@yandex.ru>
2023-07-19 14:30:28 +0400

    Update index.tsx. Remove right and middle clicks functions

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  );
+  root.render(<App />);
+  document.oncontextmenu = () => {return false}
+  document.addEventListener("mousedown", (e) => {
+    if (e.button === 1)
+      e.preventDefault()
+  })
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
