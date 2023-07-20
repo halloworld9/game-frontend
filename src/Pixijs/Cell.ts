bf79b657927b146821d7322a3358e4c9037e8977
Halloworld <prishedko.ilya@yandex.ru>
2023-07-21 02:02:21 +0400

    Now save function in field. Create methods setCliclable and setUnclickable. Change order of propherties.

--- src/Pixijs/Cell.ts
+++ src/Pixijs/Cell.ts
@@ -3,15 +3,25 @@ import { rectangle, wall } from "./Textures"
 
 
 export class Cell extends Sprite {
+    moveFn: FederatedEventHandler
 
-    constructor(isPassable: boolean, x = 0, y = 0, fn: FederatedEventHandler) {
+    constructor(isPassable: boolean, fn: FederatedEventHandler, x = 0, y = 0) {
         super(isPassable ? rectangle : wall)
         this.x = x * this.texture.width
         this.y = y * this.texture.height
         this.eventMode = "static"
-        this.on("pointerdown", fn)
+        this.moveFn = fn
     }
 
+    setClickable() {
+        this.cursor = "pointer"
+        this.on("pointerdown", this.moveFn)
+    }
+
+    setUnclickable() {  
+        this.cursor = ""
+        this.on("pointerdown", () => { })
+    }
 
 }
 
